389f1768778fbab205188b950bfe68e512ce12d0
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-24 01:58:59 -0300

    feat(home-header): :art: add home header component and separator

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { HomeHeader } from "@/components/HomeHeader";
+import { View } from "react-native";
 
 export default function Index() {
     return (
-        <View style={{flex: 1, justifyContent: 'center'}}>
-            <Text>Olá, Expo Router!</Text>
+        <View style={{flex: 1}}>
+            <HomeHeader data={{total: "R$ 2.000,00"}} />
         </View>
     )
 }
