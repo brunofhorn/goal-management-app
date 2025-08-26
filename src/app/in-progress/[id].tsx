54399469fc653021fc88e76f765c6a7bea8fb92e
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-26 01:17:46 -0300

    feat(progress bar): :art: create a progress bar component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,12 +1,23 @@
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
 import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
+const details = {
+    current: "R$ 580,00",
+    target: "R$ 1.790,00",
+    percentage: 25
+}
+
 export default function InProgress() {
     const params = useLocalSearchParams()
-    
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Text>In Progress</Text>
+        <View style={{ flex: 1, padding: 24, gap: 32 }}>
+            <PageHeader title="Apple Watch" subtitle="" rightButton={{ icon: 'edit', onPress: () => { } }} />
+            
+            
+            <Progress data={details} />
         </View>
     )
 }
