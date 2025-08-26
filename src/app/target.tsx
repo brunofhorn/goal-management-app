a68a88ec1844b0df39a768269b6c8137b0859aac
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-26 00:40:54 -0300

    feat(page header): :art: create a page header component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
+import { Button } from "@/components/Button";
+import { PageHeader } from "@/components/PageHeader";
+import { router } from "expo-router";
+import { View } from "react-native";
 
 export default function Target() {
     return (
-        <View>
-            <Text>Target!</Text>
+        <View style={{ flex: 1, padding: 24 }}>
+            <PageHeader title="Meta" subtitle="Economize para alcançar sua meta financeira." />
+
+            <Button title="Voltar" onPress={() => router.back()} />
         </View>
     )
 }
