3c50bd23d5ae046ef2dfc62c1d6a86194376ab5a
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-27 22:58:56 -0300

    feat(transaction): :art: create a transaction component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/Button";
+import { List } from "@/components/List";
 import { PageHeader } from "@/components/PageHeader";
 import { Progress } from "@/components/Progress";
-import { useLocalSearchParams } from "expo-router";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+import { TransactionTypes } from "@/utils/TransactionTypes";
+import { router, useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
 const details = {
@@ -9,15 +13,42 @@ const details = {
     percentage: 25
 }
 
+const transactions: TransactionProps[] = [
+    {
+        id: '1',
+        value: 'R$ 20,00',
+        date: '12/04/25',
+        description: '',
+        type: TransactionTypes.Output
+    },
+    {
+        id: '2',
+        value: 'R$ 300,00',
+        date: '12/04/25',
+        description: 'CDB de 110% no banco XPTO',
+        type: TransactionTypes.Input
+    }
+]
+
 export default function InProgress() {
     const params = useLocalSearchParams()
 
     return (
         <View style={{ flex: 1, padding: 24, gap: 32 }}>
             <PageHeader title="Apple Watch" subtitle="" rightButton={{ icon: 'edit', onPress: () => { } }} />
-            
-            
+
             <Progress data={details} />
+
+            <List
+                title="Transações"
+                data={transactions}
+                renderItem={({ item }) =>
+                    <Transaction data={item} onRemove={() => { }} />
+                }
+                emptyMessage="Nenhuma transação. Toque em nova transação para guardar seu primeiro dinheiro aqui."
+            />
+
+            <Button title="Nova transação" onPress={()=> router.navigate(`/transaction/${params.id}`)} />
         </View>
     )
 }
